55d351ed7dc76a76dbdbec6b66a08f557306a6f6
glyph8 <newdy0808@gmail.com>
2026-02-27 17:37:07 +0900

    feat: project detail info section and detailed animation

--- src/pages/portfolio-detail/PortfolioDetail.tsx
+++ src/pages/portfolio-detail/PortfolioDetail.tsx
@@ -5,6 +5,9 @@ import { useLocation, useNavigate } from "react-router";
 import { useProjects } from "../portfolio/projects/hooks/use-projects";
 import Loading from "../../components/loading/Loading";
 import NotFound from "../../components/error/NotFound";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+import { useEffect, useRef, useState } from "react";
 
 export default function PortfolioDetail() {
 
@@ -14,6 +17,36 @@ export default function PortfolioDetail() {
   const id = path.split("/").pop();
 
   const { projects, isLoading, isError } = useProjects();
+
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [maskState, setMaskState] = useState('right');
+
+  const isDragging = useRef(false);
+  const startX = useRef(0);
+  const scrollLeft = useRef(0);
+
+  const onDragStart = (e: React.MouseEvent) => {
+    isDragging.current = true;
+    if (!scrollRef.current) return;
+
+    scrollRef.current.style.scrollSnapType = 'none';
+    startX.current = e.pageX - scrollRef.current.offsetLeft;
+    scrollLeft.current = scrollRef.current.scrollLeft;
+  };
+
+  const onDragEnd = () => {
+    isDragging.current = false;
+    if (!scrollRef.current) return;
+    scrollRef.current.style.scrollSnapType = 'x proximity';
+  }
+
+  const onDragMove = (e: React.MouseEvent) => {
+    if (!isDragging.current || !scrollRef.current) return;
+    e.preventDefault();
+    const x = e.pageX - scrollRef.current.offsetLeft;
+    const walk = (x - startX.current) * 2; // 드래그 속도 조절
+    scrollRef.current.scrollLeft = scrollLeft.current - walk;
+  };
 
   if (isLoading) {
     return <Loading />;
@@ -27,6 +60,36 @@ export default function PortfolioDetail() {
 
   const handleBack = () => {
     navigate(-1);
+  }
+
+  const handleScroll = () => {
+    if (!scrollRef.current) return;
+    const { scrollLeft, scrollWidth, clientWidth } = scrollRef.current;
+
+    const isAtStart = scrollLeft <= 0;
+    // 오차 범위를 위해 -1 정도 여유를 줍니다.
+    const isAtEnd = scrollLeft + clientWidth >= scrollWidth - 1;
+
+    if (isAtStart) setMaskState('right');
+    else if (isAtEnd) setMaskState('left');
+    else setMaskState('both'); // 중간에 있을 때
+  };
+
+  // 컴포넌트가 처음 켜졌을 때 길이를 계산해서 상태 초기화
+  useEffect(() => {
+    handleScroll();
+  }, [project]);
+
+  const handleSkillClick = (index: number) => {
+    if (!scrollRef.current) return;
+    const targetCard = scrollRef.current.children[index] as HTMLElement;
+
+    if (targetCard) {
+      scrollRef.current.scrollTo({
+        left: targetCard.offsetLeft - scrollRef.current.offsetLeft,
+        behavior: "smooth"
+      })
+    }
   }
 
   return (
@@ -46,18 +109,33 @@ export default function PortfolioDetail() {
           <div className={styles.techStacks}>
             <h2>사용 기술 스택</h2>
             <ul>
-              {project?.project_skills.map((ps) => (
-                <li key={ps.skills.name}>{ps.skills.name}</li>
+              {project?.project_skills.map((ps, index) => (
+                <li key={ps.skills.name}
+                  onClick={() => handleSkillClick(index)}
+                  style={{ cursor: "pointer" }}
+                >{ps.skills.name}</li>
               ))}
             </ul>
           </div>
         </div>
 
         <div className={styles.projectOverview}>
-          <h2>프로젝트 슬로건</h2>
-          <p>프로젝트 소개소개소개소개개개개개</p>
+          <div>
+            <h2>프로젝트 슬로건</h2>
+            <p>프로젝트 소개소개소개소개개개개개</p>
+          </div>
 
-          <nav className={styles.techStacksContainer}>
+          <nav className={`${styles.techStacksContainer} ${maskState === 'right' ? styles.maskRight :
+              maskState === 'left' ? styles.maskLeft :
+                maskState === 'both' ? styles.maskBoth : ''
+            }`}
+            ref={scrollRef}
+            onMouseDown={onDragStart}
+            onMouseLeave={onDragEnd} // 마우스가 영역을 벗어나면 드래그 종료
+            onMouseUp={onDragEnd}    // 클릭을 떼면 드래그 종료
+            onMouseMove={onDragMove}
+            onScroll={handleScroll}
+          >
             {
               project?.project_skills.map((ps) => {
                 return (
@@ -78,7 +156,7 @@ export default function PortfolioDetail() {
       </section>
 
       <div className={styles.readme}>
-        <ReactMarkdown>
+        <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
           {project?.readme || README_CONTENT}
         </ReactMarkdown>
       </div>
