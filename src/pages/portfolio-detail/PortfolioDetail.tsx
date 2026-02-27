c30529069f1a59e757a2a47562fe7dc98a1dad72
glyph8 <newdy0808@gmail.com>
2026-02-27 16:05:38 +0900

    feat: project details rendering success

--- src/pages/portfolio-detail/PortfolioDetail.tsx
+++ src/pages/portfolio-detail/PortfolioDetail.tsx
@@ -1,41 +1,97 @@
 import ReactMarkdown from "react-markdown";
 import styles from "./PortfolioDetail.module.css";
 import { README_CONTENT } from "./constant";
+import { useLocation, useNavigate } from "react-router";
+import { useProjects } from "../portfolio/projects/hooks/use-projects";
+import Loading from "../../components/loading/Loading";
+import NotFound from "../../components/error/NotFound";
 
 export default function PortfolioDetail() {
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  const path = location.pathname;
+  const id = path.split("/").pop();
+
+  const { projects, isLoading, isError } = useProjects();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (!projects || projects.length === 0 || isError) {
+    return <NotFound />;
+  }
+
+  const project = projects.find((p) => p.project_id === Number(id));
+
+  const handleBack = () => {
+    navigate(-1);
+  }
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>포트폴리오 상세 페이지</h1>
+      <h1 className={styles.title}>{project?.title}</h1>
 
-      <div className={styles.projectInfo}>
-        <div className={styles.projectInfoItem}>
-          <h2>프로젝트 정보</h2>
-          <p>역할: 프론트엔드 개발자</p>
-          <p>기간: 2023.01 ~ 2023.06</p>
-          <p>기여도: 70%</p>
+      <section className={styles.projectHeader}>
+
+        <div className={styles.projectInfo}>
+          <div className={styles.projectInfoItem}>
+            <h2>프로젝트 정보</h2>
+            <p>역할: {project?.role}</p>
+            <p>기간: {project?.duration}</p>
+            <p>기여도: {project?.contribution}</p>
+          </div>
+
+          <div className={styles.techStacks}>
+            <h2>사용 기술 스택</h2>
+            <ul>
+              {project?.project_skills.map((ps) => (
+                <li key={ps.skills.name}>{ps.skills.name}</li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div className={styles.techStacks}>
-          <h2>사용 기술 스택</h2>
-          <ul>
-            <li>React</li>
-            <li>TypeScript</li>
-            <li>CSS Modules</li>
-          </ul>
+
+        <div className={styles.projectOverview}>
+          <h2>프로젝트 슬로건</h2>
+          <p>프로젝트 소개소개소개소개개개개개</p>
+
+          <nav className={styles.techStacksContainer}>
+            {
+              project?.project_skills.map((ps) => {
+                return (
+                  <div className={styles.techStacksCard}>
+                    <div className={styles.techStacksCardTitle}>
+                      {ps.skills.name}
+                    </div>
+                    <p className={styles.techStacksCardDescription}>
+                      {ps.skill_reason}
+                    </p>
+                  </div>
+                )
+              })
+            }
+          </nav>
         </div>
-      </div>
+
+      </section>
 
       <div className={styles.readme}>
         <ReactMarkdown>
-        {README_CONTENT}
+          {project?.readme || README_CONTENT}
         </ReactMarkdown>
       </div>
 
       <nav className={styles.actions}>
-        <button>
+        <button onClick={handleBack}>
           목록으로
         </button>
         <button>
           수정하기
+        </button>
+        <button>
+          삭제하기
         </button>
       </nav>
 
--- src/pages/portfolio/projects/components/ProjectCard.tsx
+++ src/pages/portfolio/projects/components/ProjectCard.tsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 
 
 export default function ProjectCard(props: ProjectCardProps) {
+    
     return (
         <article className={styles.card}>
             <div className={styles.imageWrapper}>
